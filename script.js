79cf6e158ed1914efc0eb1084aec7a6d2390c466
JGonz13x <154345982+JGonz13x@users.noreply.github.com>
2024-10-24 19:18:26 +0800

    added all the rock paper scissor functionality

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let computerScore = 0;
+let playerScore = 0;
+let roundWinner = "";
+
+getPlayerChoice();
+
 function getComputerChoice() {
   const computerChoice = Math.random();
 
@@ -10,4 +16,68 @@ function getComputerChoice() {
   }
 }
 
-console.log(getComputerChoice());
+function getPlayerChoice() {
+  const rockElement = document.querySelector(".rock");
+  const paperElement = document.querySelector(".paper");
+  const scissorElement = document.querySelector(".scissor");
+
+  rockElement.addEventListener("click", () => {
+    playRound("Rock");
+    playGame();
+  });
+  paperElement.addEventListener("click", () => {
+    playRound("Paper");
+    playGame();
+  });
+  scissorElement.addEventListener("click", () => {
+    playRound("Scissor");
+    playGame();
+  });
+}
+
+function playRound(playerChoice) {
+  const computerChoice = getComputerChoice();
+
+  if (playerChoice === "Rock" && computerChoice === "Paper") {
+    roundWinner = "Computer Wins!";
+    computerScore++;
+  } else if (playerChoice === "Rock" && computerChoice === "Scissor") {
+    roundWinner = "Player Wins";
+    playerScore++;
+  } else if (playerChoice === "Paper" && computerChoice === "Rock") {
+    roundWinner = "Player Wins";
+    playerScore++;
+  } else if (playerChoice === "Paper" && computerChoice === "Scissor") {
+    roundWinner = "Computer Wins!";
+    computerScore++;
+  } else if (playerChoice === "Scissor" && computerChoice === "Rock") {
+    roundWinner = "Computer Wins!";
+    computerScore++;
+  } else if (playerChoice === "Scissor" && computerChoice === "Paper") {
+    roundWinner = "Player Wins";
+    playerScore++;
+  } else {
+    roundWinner = "Draw!";
+  }
+  document.querySelector(
+    ".score"
+  ).innerHTML = `Player Score: ${playerScore} Computer Score: ${computerScore}`;
+}
+
+document.querySelector(
+  ".score"
+).innerHTML = `Player Score: ${playerScore} Computer Score: ${computerScore}`;
+
+function playGame() {
+  if (playerScore > computerScore && playerScore === 5) {
+    document.querySelector(".winner").innerHTML = roundWinner;
+    playerScore = 0;
+    computerScore = 0;
+  } else if (playerScore < computerScore && computerScore === 5) {
+    document.querySelector(".winner").innerHTML = roundWinner;
+    playerScore = 0;
+    computerScore = 0;
+  } else {
+    document.querySelector(".winner").innerHTML = "";
+  }
+}
